5c45f010aa3a86776c3248582addc0c5d1166f56
Jose Levy <clic00@yahoo.com>
2017-12-09 22:55:00 -0300

    Inserindo Pedido

--- src/main/java/com/clic00/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/clic00/cursomc/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.clic00.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.clic00.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
