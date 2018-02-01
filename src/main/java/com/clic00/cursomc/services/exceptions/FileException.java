df10e65725404707189f1b9b85d481ce9133ecac
Jose Levy <clic00@yahoo.com>
2018-01-31 21:10:46 -0300

    Tratando exceções adequadamente

---
+++ src/main/java/com/clic00/cursomc/services/exceptions/FileException.java
@@ -0,0 +1,15 @@
+package com.clic00.cursomc.services.exceptions;
+
+public class FileException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+		public FileException(String msg) {
+			super(msg);
+			
+		}
+	
+		public FileException(String msg, Throwable cause) {
+			super(msg,cause);
+			
+		}
+}
