4e2fadc781d722cbc60209a6e58fbe20e05c12c5
Jose Levy <clic00@yahoo.com>
2018-02-04 16:55:56 -0300

    Expondo o header Authorization

--- src/main/java/com/clic00/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/clic00/cursomc/security/JWTAuthenticationFilter.java
@@ -67,6 +67,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String token = jwtUtil.generateToken(username);
 		
 		response.addHeader("Authorization","Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
 	}
 
 	@Override
